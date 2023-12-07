46d99342b3bcae88fd6678ca5f1be8716f29d58a
Tanmaya Sahoo <tanmayasahoo426813@gmail.com>
2023-12-07 18:29:35 +0530

    Pre deployment code done

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
+import path from 'path';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -11,7 +12,11 @@ mongoose.connect(process.env.MONGO)
 .then(()=>{console.log("connection successful")})
 .catch((e)=>{console.log(e)})
 
+//creating dynamic directory name
+const __dirname =path.resolve();
+
 const app=express();
+
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000, ()=>{console.log('server is running on port 3000') })
@@ -20,6 +25,12 @@ app.listen(3000, ()=>{console.log('server is running on port 3000') })
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));                  // In case of create react app  /client/build
+
+app.get('*',(req,res)=>{        //Any other url send below file index.html
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
